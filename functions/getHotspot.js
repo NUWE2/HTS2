f9b841513b0ad5fcb1c54f4690653d6e565e8e36
Mark <mark.sanjuan.sole@gmail.com>
2021-04-04 16:35:06 +0200

    added hotspot

---
+++ functions/getHotspot.js
@@ -0,0 +1,16 @@
+const Pha = require('../models/pha.model')
+
+const getHotspot = async (clients) => {
+    const newClientsArr = clients.map(client => {
+        const newLat0 = client.latitude - 15
+        const newLat1 = client.latitude + 15
+        const newLong0 = client.longitude - 15
+        const newLong1 = client.longitude + 15
+        const phas = await Pha.find({ $and: [ { latitude: { $lte: newLat0 } },
+        { latitude: { $gte: newLat1 } },
+        { longitude: { $lte: newLong0 } },
+        { longitude: { $gte: newLong1 } } ] })
+        client.hotspot_asteroids = phas.length
+    })
+}
+module.exports = getHotspot
