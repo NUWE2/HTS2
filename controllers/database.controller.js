7b561dade51ce0bc630e851c57050bf41d5734b0
Miguel Angel Rincon <miguel.angel.rincon@estudiantat.upc.edu>
2021-04-04 11:57:30 +0200

    Update database.controller.js

--- controllers/database.controller.js
+++ controllers/database.controller.js
@@ -1,14 +1,19 @@
 const csvtojson = require('csvtojson')
 const PHA = require('../models/pha.model')
 const Client = require('../models/client.model')
-
-const csvPhaFilePath =  "../assets/OrbtialParameters_PHAs_1.csv"
+const path = require('path')
+const csvPhaFilePath =  "../assets/OrbitalParameters_PHAs_1.csv"
 const csvClientsFilePath =  "../assets/List_Of_Clients.csv"
 
-csvtojson().fromFile(csvPhaFilePath).then(async(json) =>{
-    await PHA.insertMany(json);
- })
 
- csvtojson().fromFile(csvClientsFilePath).then(async(json) =>{
-    await Client.insertMany(json);
- })
+  
+    export const phaCsvToJson = async () => {
+        return await csv().fromFile(path.join(__dirname, csvPhaFilePath))
+    }
+
+   export const clientCsvToJson = async () => {
+        return await csv().fromFile(path.join(__dirname, csvClientsFilePath))
+    }
+
+   
+
