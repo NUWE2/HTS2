2889097d1a96516cc6c187b640e174a4c3e5c904
JonatanRomero <jonatanrm35@gmail.com>
2021-04-04 21:35:32 +0200

    getclient

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Rutas
 router.get('/clients', clientRoutes.getAllClients);
 router.get('/save-clients-from-csv', clientRoutes.saveClientsFromCSV);
-//router.get('/clients/:id', getClient);
+router.get('/clients/:id', clientRoutes.getClient);
 // router.put('/clients/:id', update);
 // router.delete('/clients/:id', delete);
 // router.get('/clients/search/:search', search)
